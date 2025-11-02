60bf55bfae8d39c53a231e2799e97e3559ef0f58
Zentanox <goddeadpokemon@gmail.com>
2025-11-02 15:47:25 +0700

    Push project to github on branch develop

---
+++ src/main/java/com/poly/perspringbootprojectmvn/PerSpringBootProjectMvnApplication.java
@@ -0,0 +1,13 @@
+package com.poly.perspringbootprojectmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerSpringBootProjectMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerSpringBootProjectMvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/perspringbootprojectmvn/PerSpringBootProjectMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.perspringbootprojectmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerSpringBootProjectMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
